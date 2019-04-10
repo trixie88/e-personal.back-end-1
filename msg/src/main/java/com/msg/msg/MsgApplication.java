c830d1e706ab0c904a9d9f953bf63f0614714fdc
Theo Bouras <damcbour@hotmail.com>
2019-04-10 18:02:31 +0300

    upload files controllers - services etc added

--- msg/src/main/java/com/msg/msg/MsgApplication.java
+++ msg/src/main/java/com/msg/msg/MsgApplication.java
@@ -2,8 +2,12 @@ package com.msg.msg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.msg.msg.property.FileStorageProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({FileStorageProperties.class})
 public class MsgApplication {
 
 	public static void main(String[] args) {
